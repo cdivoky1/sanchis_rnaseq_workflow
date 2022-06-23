cdb37516680a293bd4445bbf343200f96fe1bdde
cdivoky1 <107935739+cdivoky1@users.noreply.github.com>
2022-06-23 16:25:34 -0400

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -5,9 +5,9 @@ install.packages("ggplot2") # for plotting
 install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
 
 ### install bioconductor packages
-# install.packages("BiocManager")
-# BiocManager::install("package")
+install.packages("BiocManager")
+BiocManager::install("package")
 
 ### install GitHub packages (tag = commit, branch or release tag)
-# install.packages("devtools")
-# devtools::install_github("user/repo", ref = "tag")
+install.packages("devtools")
+devtools::install_github("user/repo", ref = "tag")
