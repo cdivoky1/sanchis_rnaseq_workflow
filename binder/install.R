4e398259791bce2909e6ee5ef12c32d765aacfcd
cdivoky1 <107935739+cdivoky1@users.noreply.github.com>
2022-06-23 16:12:25 -0400

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -2,12 +2,12 @@
 
 #install.packages("reticulate") # python support in RMarkdown
 #install.packages("ggplot2") # for plotting
-#install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
+install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
 
 ### install bioconductor packages
-# install.packages("BiocManager")
-# BiocManager::install("package")
+install.packages("BiocManager")
+BiocManager::install("package")
 
 ### install GitHub packages (tag = commit, branch or release tag)
-# install.packages("devtools")
-# devtools::install_github("user/repo", ref = "tag")
+install.packages("devtools")
+devtools::install_github("user/repo", ref = "tag")
